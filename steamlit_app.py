49289301df8086c820db8d937c374d65fc55e445
Pushpak Mahajan <50670071+pushpak-mahajan@users.noreply.github.com>
2023-04-30 13:48:43 +0530

    Update steamlit_app.py
    Add menu , header, text

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,10 @@
 
 import streamlit
-streamlit.title ('Hrllo world !! ')
+streamlit.title ('Welcome to Pushpak Repo ')
+streamlit.header('Breakfast Fevorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avecado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
