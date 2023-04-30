927fc0be752b18caa154d8995294d1b58495a41b
Pushpak Mahajan <50670071+pushpak-mahajan@users.noreply.github.com>
2023-04-30 13:50:29 +0530

    Update steamlit_app.py
    Import pandas

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 streamlit.title ('Welcome to Pushpak Repo ')
 streamlit.header('Breakfast Fevorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -8,3 +9,6 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avecado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
